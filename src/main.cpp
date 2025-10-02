8277454792836fac6d12a0e9ca23c58624dc78dd
Justin Stachurski <stachuj@sunypoly.edu>
2025-10-01 20:49:30 -0400

    Added some comments and implemented game loop logic.

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,50 @@
 #include "raylib.h"
-#include "basic_object.hpp"
+#include "game_object.hpp"
+#include "test_object.hpp"
+#include <vector>
 
-#define MAX_OBJECTS 100
-
-static BasicObject _objects[MAX_OBJECTS] = {0};
-int objectCount = 0;
+using namespace std;
 
 int main() {
 
+	// list of all objects that will be updated and drawn
+	// they are pointers so we can have subclasses in the vector
+	vector<GameObject *> objects;
+
+	// Window size
 	InitWindow(800, 600, "CS512 Funtime");
+
+	// We will draw a new frame to the screen 60 times per second
+	// This will NOT limit physics to 60 ticks per second, so we have to use deltaTime!
 	SetTargetFPS(60);
 
-	Vector2 pos = {400.0,300.0};
-	_objects[objectCount++] = BasicObjectCreate(pos);
+	// Adding a new object to the world
+	// Will have to come up with a nicer way to create and remove objects from the world...
+	objects.push_back(new TestObject({100.0, 400.0}));
 	
 	while (!WindowShouldClose()) {
 
-		for (int i = 0; i < objectCount; i++) {
-			BasicObjectUpdate(&_objects[i], GetFrameTime());
-		}
+		// This is the real time in seconds since the last update
+		float deltaTime = GetFrameTime();
+
+		// For every object in objects, call their update function (passing deltaTime)
+		for (auto object: objects)
+			object->Update(deltaTime);
 		
 		BeginDrawing();
 		{
 			ClearBackground(RAYWHITE);
-			
-			for (int i = 0; i < objectCount; i++) {
-				BasicObjectDraw(_objects[i]);
-			}
 
-			DrawText("Hi team members...!\nDo not be slacking off.\nRemember the importance of SCRUM's principalz.", 10, 10, 32, BLACK);
+			// For every object in objects, call their draw function
+			for (auto object: objects)
+				object->Draw();
+			
+			DrawText("This is Jeremy the purple square.", 10, 10, 32, BLACK);
 			
 		}
 		EndDrawing();
 		
 	}
-	
 	CloseWindow();
 
     return 0;
