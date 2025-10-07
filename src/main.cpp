461baf9c9c981f772e1b67b1418ee0d4d85d9bfe
Mian Danyal <danyalm@sunypoly.edu>
2025-10-07 14:17:18 -0400

    Character animations and movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,35 @@
 #include "raylib.h"
 #include "game_object.hpp"
-#include "test_object.hpp"
+#include "character.hpp"
 #include <vector>
 
-using namespace std;
-
 int main() {
+    InitWindow(800, 600, "Animated Character Demo");
+    SetTargetFPS(60);
 
-	// list of all objects that will be updated and drawn
-	// they are pointers so we can have subclasses in the vector
-	vector<GameObject *> objects;
+    std::vector<GameObject*> objects;
 
-	// Window size
-	InitWindow(800, 600, "CS512 Funtime");
+    Character* player = new Character({400, 300}, "assets");
+    objects.push_back(player);
 
-	// We will draw a new frame to the screen 60 times per second
-	// This will NOT limit physics to 60 ticks per second, so we have to use deltaTime!
-	SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        float deltaTime = GetFrameTime();
 
-	// Adding a new object to the world
-	// Will have to come up with a nicer way to create and remove objects from the world...
-	objects.push_back(new TestObject({100.0, 400.0}));
-	
-	while (!WindowShouldClose()) {
+        for (auto obj : objects)
+            obj->Update(deltaTime);
 
-		// This is the real time in seconds since the last update
-		float deltaTime = GetFrameTime();
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
 
-		// For every object in objects, call their update function (passing deltaTime)
-		for (auto object: objects)
-			object->Update(deltaTime);
-		
-		BeginDrawing();
-		{
-			ClearBackground(RAYWHITE);
+        for (auto obj : objects)
+            obj->Draw();
 
-			// For every object in objects, call their draw function
-			for (auto object: objects)
-				object->Draw();
-			
-			DrawText("This is Jeremy the purple square.", 10, 10, 32, BLACK);
-			
-		}
-		EndDrawing();
-		
-	}
-	CloseWindow();
+        EndDrawing();
+    }
 
+    for (auto obj : objects)
+        delete obj;
+
+    CloseWindow();
     return 0;
 }
