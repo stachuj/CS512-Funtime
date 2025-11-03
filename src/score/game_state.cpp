603805c48175d8383703ad006b2cdff9a6f7d5d3
Justin Stachurski <stachuj@sunypoly.edu>
2025-11-03 14:51:37 -0500

    Turned into C++ class instead of a struct. Moved timer update code into a function called from main

---
+++ src/score/game_state.cpp
@@ -0,0 +1,12 @@
+#include "game_state.hpp"
+
+void GameState::UpdateTimer(float deltaTime) {
+    // ADDED: countdown timer
+    if (!this->timeUp) {
+        this->timeRemaining -= deltaTime;
+        if (this->timeRemaining <= 0.0f) {
+            this->timeRemaining = 0.0f;
+            this->timeUp = true;
+        }
+    }
+}
--- src/score/game_state.hpp
+++ src/score/game_state.hpp
@@ -1,8 +1,11 @@
 #pragma once
-struct GameState {
-    int   score = 0;
-    float timeLimit = 60.0f;
-    float timeRemaining = timeLimit;
-    bool  timeUp = false;
-    int   pointsPerCollectible = 10;
+class GameState {
+    public:
+        int score = 0;
+        float timeLimit = 60.0f;
+        float timeRemaining = timeLimit;
+        bool timeUp = false;
+        int pointsPerCollectible = 10;
+
+        void UpdateTimer(float deltaTime);
 };
