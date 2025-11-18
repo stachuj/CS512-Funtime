0cd468c5d672ed741db4e1d1ff9bf057ed40a695
Justin Stachurski <stachuj@sunypoly.edu>
2025-11-18 18:39:37 -0500

    Added dropshadow to HUD

--- src/score/hud.hpp
+++ src/score/hud.hpp
@@ -3,11 +3,13 @@
 #include "game_state.hpp"
 
 inline void DrawHUD(const GameState& gs) {
+    DrawText(TextFormat("SCORE: %d", gs.score), 10+2, 10+2, 28, BLACK);
     DrawText(TextFormat("SCORE: %d", gs.score), 10, 10, 28, WHITE);
     const int fontSize = 28;
     int seconds = (int)ceilf(gs.timeRemaining);
     const char* t = TextFormat("TIME: %02d", seconds);
     int w = MeasureText(t, fontSize);
+    DrawText(t, GetScreenWidth()/2 - w/2 + 2, 10+2, fontSize, BLACK);
     DrawText(t, GetScreenWidth()/2 - w/2, 10, fontSize, WHITE);
     if (gs.timeUp) {
         const char* msg = "TIME'S UP! PRESS R TO RESTART";
